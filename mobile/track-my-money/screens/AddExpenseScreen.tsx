a510a7a04063c43984d6064e09b63fb5503b32d4
Marcin Bigos <marcin.bigos99@gmail.com>
2021-09-04 23:53:41 +0200

    save to db

--- mobile/track-my-money/screens/AddExpenseScreen.tsx
+++ mobile/track-my-money/screens/AddExpenseScreen.tsx
@@ -1,12 +1,26 @@
-import * as React from "react";
-import { StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { Alert, Button, StyleSheet, TextInput } from "react-native";
 
 import { Text, View } from "../components/Themed";
 
+import * as FileSystem from "expo-file-system";
+
 export default function AddExpenseScreen() {
+  const [state, setState] = useState({ name: "test" });
+
+  const saveExpense = async () => {
+    console.log("expense saved");
+    let fileName = FileSystem.documentDirectory + "expense";
+    await FileSystem.writeAsStringAsync(fileName, state.name);
+    let result = await FileSystem.readAsStringAsync(fileName);
+    Alert.alert("Saved", result);
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>AddExpenseScreen </Text>
+      <Text style={styles.title}>Name</Text>
+      <TextInput style={styles.input} value={state.name} onChangeText={(x) => setState({ ...state, name: x })} />
+      <Button title="Add expense" onPress={saveExpense} />
     </View>
   );
 }
@@ -25,5 +39,12 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: "80%",
+  },
+  input: {
+    height: 40,
+    minWidth: 200,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
